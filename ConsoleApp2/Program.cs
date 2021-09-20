f9cbc48eafe1a0c8607785654902c6927fb247d3
nur-987 <76473637+nur-987@users.noreply.github.com>
2021-09-20 23:17:06 +0800

    Properties

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    /// <summary>
+    /// properties example
+    /// using get and set
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item item = new Item("Pen", 1.5);
+            Item item2 = new Item("Juice", 3.5);
+            Console.WriteLine(item);
+            Console.ReadLine();
+        }
+    }
+    class Item
+    {
+        public string Name { get; set; }
+        public double price { get; set; }
+        public Item(string name, double price)
+        {
+            this.Name = name;
+            this.price = price;
+
+        }
+
+    }
+}
