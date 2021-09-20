f06b754a9cd5a46d83b2f991a2fa4cffdb348352
nur-987 <76473637+nur-987@users.noreply.github.com>
2021-09-20 22:57:14 +0800

    Push, Pop, Clone on a Stack

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// STACK pop and push
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack myStack = new Stack();
+            myStack.Push("James");
+            myStack.Push("Henry");
+            myStack.Push("Sam");
+
+            foreach (string x in myStack)
+            {
+                Console.WriteLine(x);
+            }
+
+            myStack.Pop();
+            Console.WriteLine("After pop");
+            foreach (string x in myStack)
+            {
+                Console.WriteLine(x);
+            }
+
+            myStack.Push("Haily");
+            myStack.Push("Ben");
+            myStack.Push("Josh");
+
+            Console.WriteLine("Added new names");
+
+            Stack myStack2 = (Stack)myStack.Clone();
+
+            Console.WriteLine("After clone");
+            foreach (string x in myStack2)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
