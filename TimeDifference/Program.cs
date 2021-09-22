601dbf3f1c53c20c5323ce446ed002e048c23ae6
nur-987 <76473637+nur-987@users.noreply.github.com>
2021-09-22 20:48:03 +0800

    TimeDifference

---
+++ TimeDifference/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeDifference
+{
+    /// <summary>
+    /// Find the time difference between SG and another time zone.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time1 = DateTime.Now;
+            DateTime time2 = new DateTime(2021, 09, 22, 15, 00, 00);
+            TimeSpan timeDif = time1 - time2;
+            Console.WriteLine("Total time diference is: " + timeDif.TotalHours);
+            Console.ReadLine();
+        }
+
+    }
+}
